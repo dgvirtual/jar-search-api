67024fdbe24abe2c1b6b808f2c3d8c0300afb55c
Donatas Glodenis <dgvirtual@akl.lt>
2024-07-11 19:46:59 +0300

    move misplaced function

--- back/api-functions.php
+++ back/api-functions.php
@@ -190,40 +190,6 @@ function scrapHtml(int $id, $date = null)
     return ['new_events' => $documentArray, 'queries' => $remaining, 'success' => $success];
 }
 
-function getPageContent($url)
-{
-
-    if (function_exists('curl_init')) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt(
-            $ch,
-            CURLOPT_RETURNTRANSFER,
-            TRUE
-        );
-        curl_setopt(
-            $ch,
-            CURLOPT_HEADER,
-            FALSE
-        );
-        curl_setopt(
-            $ch,
-            CURLOPT_SSL_VERIFYHOST,
-            0
-        );
-        curl_setopt(
-            $ch,
-            CURLOPT_SSL_VERIFYPEER,
-            0
-        );
-        $response = curl_exec($ch);
-        curl_close($ch);
-    } else {
-        echo $url;
-        $response = file_get_contents($url);
-    }
-    return $response;
-}
 
 /**unfinished and unused */
 function checkGetParam($param, $value = '', $operator = '=')
