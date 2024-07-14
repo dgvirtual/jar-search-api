c8cf6c82d237de3ad9965e6c2b9f1882cdd30227
Donatas Glodenis <dgvirtual@akl.lt>
2024-07-14 23:06:46 +0300

    fix typo

--- common/classes.php
+++ common/classes.php
@@ -9,7 +9,7 @@ class mySQlite3 extends SQLite3
 
         // try to load ICU extension, if not available, use (slower) PHP functions
         // for compatibility with mysql, call collation utf8_lithuanian_ci
-        if ($this->loadExtension('libSqliteIcus.so')) {
+        if ($this->loadExtension('libSqliteIcu.so')) {
             $this->exec("SELECT icu_load_collation('lt', 'utf8_lithuanian_ci')");
         } else {
             $this->createCollation('utf8_lithuanian_ci', function ($a, $b) {
