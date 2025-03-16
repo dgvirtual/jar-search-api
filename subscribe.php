cb2d740d8edaaba118d41021fd05adfbc34f8b4b
Donatas Glodenis <dg@lapas.info>
2025-03-16 21:47:08 +0200

    Disable errors

--- subscribe.php
+++ subscribe.php
@@ -1,7 +1,7 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
 require_once(__DIR__ . '/config.php');
 require_once(BASE_DIR . 'common/functions.php');
 require_once(BASE_DIR . 'back/api-functions.php');
